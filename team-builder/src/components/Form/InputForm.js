abf1fa79a0a63120cfa13d6d57fee7273d1a3b99
Sunil Karki <karkisunil1200@gmail.com>
2019-11-13 14:35:46 -0600

    Starting project

---
+++ team-builder/src/components/Form/InputForm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const InputForm = () => {
+    
+
+    return (
+        <div>
+            <h1>Welcome to input form</h1>
+            <form>
+
+                <input type="text" name="name" onChange={} value={} />
+            </form>
+        </div>
+
+    )
+}
